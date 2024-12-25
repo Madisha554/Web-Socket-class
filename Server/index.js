4eef36d86c5c0c98a03652ba48df594091277d01
Madiso Melese <madishamadiso00@gmail.com>
2024-12-25 23:10:16 +0300

    Commit after updating secket.io

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,21 @@
-const ws = require('ws');
-const server = new ws.Server({ port: 3000 });
-server.on('connection', socket => {
-  socket.on('message', message => {
-    const buffer = Buffer.from(message);
-    console.log('Received:', buffer.toString());
-    socket.send(`${message}`);
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+const httpServer = createServer();
+
+const io = new Server(httpServer, { 
+  cors: {
+    origin: process.env.NODE_ENV === 'production' ? false: ['http://localhost:5500', 'http://127.0.0.1:5500']
+  }
+});
+io.on('connection', socket => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on('message', data => {
+    console.log(data);
+    io.emit('message', `${socket.id.substring(0, 5)}:${data}`);
   });
+});
+
+httpServer.listen(3500, () => {
+  console.log('Server listening on port 3500');
 });
